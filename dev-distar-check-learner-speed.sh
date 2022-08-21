9160a9f4b4ba55b1f5a9c0a5fec6833c03035cd7
zhumengshen <zhumengshen@pjlab.org.cn>
2022-08-21 10:05:50 +0000

    fix bug

--- dev-distar-check-learner-speed.sh
+++ dev-distar-check-learner-speed.sh
@@ -7,4 +7,4 @@ source /mnt/cache/share/spring/s0.3.4
 export SC2PATH="/mnt/lustre/zhumengshen.vendor/StarCraftII_4.10.0"
 srun --partition=cpu --job-name=league_learner_speed -w $1 --mpi=pmi2 --async --output=./check-learner-speed-logs/league_output_check_learner_speed.txt python -m distar.bin.rl_train --type league --task selfplay --coordinator_ip $2
 srun --partition=cpu --job-name=coordinator_learner_speed -w $1 --mpi=pmi2 --async --output=./check-learner-speed-logs/coordinator_output_check_learner_speed.txt python -m distar.bin.rl_train --type coordinator --coordinator_ip $2
-srun --partition=GAME -N 2 --gres=gpu:8 --ntasks-per-node 8 -c 256 --job-name=learner_learner_speed -w SH-IDC1-10-198-34-[142,146] --mpi=pmi2 --async --output=./check-learner-speed-logs/learner_output_check_learner_speed.txt python -m distar.bin.rl_train --type learner --coordinator_ip $2
+srun --partition=GAME -N 2 --gres=gpu:8 --ntasks-per-node 8 -c 32 --job-name=learner_learner_speed -w SH-IDC1-10-198-34-[142,146] --mpi=pmi2 --async --output=./check-learner-speed-logs/learner_output_check_learner_speed.txt python -m distar.bin.rl_train --type learner --coordinator_ip $2
